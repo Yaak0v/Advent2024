4ab641aa906c22b541d09f680676d05d7958d1b6
Yaak0v <jacobsandry@gmail.com>
2024-12-21 22:36:40 -0500

    Get an array of arrays

--- src/day2/solution.ts
+++ src/day2/solution.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 
 const input = fs.readFileSync("src/day2/input.txt", "utf-8");
-const nuclearData = input.split('\n')
-console.log(nuclearData)
+const reports: string[] = input.split('\n')
+const levels: string[][] = reports.map((value) => value.split(' ')) 
+
+console.log(levels)
