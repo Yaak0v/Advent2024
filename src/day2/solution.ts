926888d84188be1018202048f2d47d4ccf103c7d
Yaak0v <jacobsandry@gmail.com>
2024-12-22 20:21:50 -0500

    Sort of angry and moving on from Day 2

--- src/day2/solution.ts
+++ src/day2/solution.ts
@@ -3,13 +3,6 @@ import fs from 'fs';
 const input = fs.readFileSync('src/day2/inputtextshort.txt', 'utf-8');
 const reports: string[] = input.split('\n');
 const levels = reports.map((value) => value.split(' ').map(Number));
-const multiLevels = levels.map((value, index, array) => {
-  return value.map((value, index, array) => {
-    return array.toSpliced(index, 1);
-  });
-});
-
-console.log(multiLevels);
 
 const safeLevels = levels.reduce((accumulator, currentValue, index) => {
   // Determine start direction
@@ -53,3 +46,47 @@ const safeLevels = levels.reduce((accumulator, currentValue, index) => {
 }, 0);
 
 console.log(`There are ${safeLevels} safe reports`);
+
+// Work on multiple levels
+const multiLevels = levels.map((value, index, array) => {
+  return value.map((value, index, array) => {
+    return array.toSpliced(index, 1);
+  });
+});
+console.log(multiLevels);
+
+const safeMultiLevels = multiLevels.reduce(
+  // First Level
+  (previousValue, currentValueFL, index, array) => {
+    let safe: boolean = true;
+    // Second Level
+    currentValueFL.reduce((accumulator, currentValueSL, index, array) => {
+      //Third Level
+      currentValueSL.reduce((prevValue, stepValue, index, array) => {
+        console.log(`${prevValue}, ${stepValue}, ${array}`);
+        let stepDirection: 'increasing' | 'decreasing' | 'flat';
+        if (index !== 0) {
+          let delta = prevValue - stepValue;
+          if (delta > 0) {
+            stepDirection = 'decreasing';
+          } else if (delta < 0) {
+            stepDirection = 'increasing';
+          } else {
+            stepDirection = 'flat';
+          }
+          // if (stepDirection !== direction) {
+          //   safe = false;
+          //   return stepValue;
+          // } else if (Math.abs(delta) > 3) {
+          //   safe = false;
+          //   return stepValue;
+        }
+        return stepValue;
+      }, currentValueSL[0]);
+      return currentValueSL;
+    });
+    return currentValueFL;
+  }
+);
+
+console.log(multiLevels);
