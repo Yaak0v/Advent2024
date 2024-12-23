0312a07570b106bb57be68e44dedb81eb38eb41c
Yaak0v <jacobsandry@gmail.com>
2024-12-22 22:14:42 -0500

    Part 2 but it's not working

--- src/day3/solution.ts
+++ src/day3/solution.ts
@@ -3,14 +3,46 @@ import fs from 'fs';
 const input = fs.readFileSync('src/day3/input.txt', 'utf-8');
 
 const cleanMultipleStrings = input.match(/mul\((\d{1,3}),(\d{1,3})\)/g);
-const multipliedValues =
-  cleanMultipleStrings?.map((value, index, array) => {
-    let numberPair = value.match(/\d{1,3}/g)?.map(Number);
+
+const createNumberPair = (v: string) => {
+  return v.match(/\d{1,3}/g)?.map(Number);
+};
+
+const totalValue = cleanMultipleStrings
+  ?.map((value) => {
+    let numberPair = createNumberPair(value);
     if (numberPair && numberPair.length == 2) {
       return numberPair[0] * numberPair[1];
     } else return 0;
-  }) ?? [];
-const totalValue = multipliedValues?.reduce((accumulator, value) => {
-  return accumulator + value;
-}, 0);
+  })
+  ?.reduce((accumulator, value) => accumulator + value, 0);
 console.log(totalValue);
+// part 2
+// split by don't()
+const splitString = input.split(`don't()`);
+// get all the do() values
+const doOnlyMuls = splitString
+  .map((value) => value.match(/(?<=do\(\).*?)mul\(\d{1,3},\d{1,3}\)/g))
+  .filter((match) => match !== null)
+  .flat();
+const doOnlyTotal = doOnlyMuls
+  ?.map((value) => {
+    let numberPair = createNumberPair(value);
+    if (numberPair && numberPair.length == 2) {
+      return numberPair[0] * numberPair[1];
+    } else return 0;
+  })
+  ?.reduce((accumulator, value) => accumulator + value, 0);
+//get the values from the first array
+const firstArrayTotal =
+  splitString[0]
+    .match(/mul\((\d{1,3}),(\d{1,3})\)/g)
+    ?.map((value) => {
+      let numberPair = createNumberPair(value);
+      if (numberPair && numberPair.length == 2) {
+        return numberPair[0] * numberPair[1];
+      } else return 0;
+    })
+    ?.reduce((accumulator, value) => accumulator + value, 0) ?? 0;
+const part2Total = firstArrayTotal + doOnlyTotal;
+console.log(part2Total);
